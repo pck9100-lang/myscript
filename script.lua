ab73812a21464a2108c1c614fd5d493a43172fa4
pck9100-lang <pck9100@gmail.com>
2026-07-20 09:09:06 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,5 @@
+local player = game.Players.LocalPlayer
+local gui = Instance.new("ScreenGui")
+local hitboxButton = Instance.new("TextButto@1+1=11:@LUIZINHO:local player = game.Players.LocalPlayer
+local gui = Instance.new("ScreenGui")
+local hitboxButton = Instance.new("TextButto
